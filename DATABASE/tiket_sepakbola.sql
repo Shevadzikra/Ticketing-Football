1c9e2507c4a7816141e898c0dfaa415c011c5e94
Lavianzy <shevaihsan5@gmail.com>
2025-05-04 07:46:13 +0700

    change database file

--- DATABASE/tiket_sepakbola.sql
+++ DATABASE/tiket_sepakbola.sql
@@ -30,8 +30,9 @@ CREATE TABLE `matches` (
   `stadium` varchar(100) NOT NULL,
   `description` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_completed` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +41,7 @@ CREATE TABLE `matches` (
 
 LOCK TABLES `matches` WRITE;
 /*!40000 ALTER TABLE `matches` DISABLE KEYS */;
-INSERT INTO `matches` VALUES (1,'Real Madrid','Barcelona FC','2025-05-16 01:55:17','Stadion Diponegoro','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.','2025-04-29 01:55:33'),(2,'Persib Bandung','Arema FC','2025-12-01 19:00:00','Stadion Gelora Bandung Lautan Api','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.','2025-04-29 02:01:36'),(3,'Persija Jakarta','Bali United','2024-01-05 20:30:00','Stadion GBK','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.','2025-04-29 02:01:36'),(5,'Tim Boyboyboi','Tim Upin Ipin','2025-05-10 14:22:00','Stadion Hura Hura','Lorem Ipsum','2025-04-29 07:22:42'),(6,'Tim Loerm','Tip Ipsum','2025-05-10 15:10:00','Stadion Hura Hura','Lorem ipsum','2025-04-29 08:10:42');
+INSERT INTO `matches` VALUES (1,'Thunder FC','Sky Hawks','2025-05-10 16:00:00','National Stadium','Big match penentuan juara liga.','2025-05-01 03:30:00',0),(2,'Ocean Warriors','Blazing Tigers','2025-05-12 19:00:00','Seaside Arena','Pertandingan sengit antara rival sekota.','2025-05-02 04:00:00',0),(3,'Sky Hawks','Golden Eagles','2025-05-15 14:30:00','National Stadium','Pertandingan semi-final yang ditunggu.','2025-05-03 02:45:00',0),(4,'Thunder FC','Ocean Warriors','2025-04-28 18:00:00','Thunder Park','Laga seru dengan peluang besar bagi Thunder FC.','2025-04-20 05:00:00',0),(5,'Blazing Tigers','Golden Eagles','2025-04-30 20:00:00','Tiger Arena','Pertandingan untuk memperebutkan posisi 3.','2025-04-21 07:20:00',0);
 /*!40000 ALTER TABLE `matches` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,17 +56,14 @@ CREATE TABLE `order_items` (
   `id` int NOT NULL AUTO_INCREMENT,
   `order_id` int NOT NULL,
   `ticket_id` int NOT NULL,
-  `seat_id` int DEFAULT NULL,
   `quantity` int NOT NULL,
   `price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`),
   KEY `ticket_id` (`ticket_id`),
-  KEY `seat_id` (`seat_id`),
-  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `order_items_ibfk_3` FOREIGN KEY (`seat_id`) REFERENCES `seats` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +72,7 @@ CREATE TABLE `order_items` (
 
 LOCK TABLES `order_items` WRITE;
 /*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
-INSERT INTO `order_items` VALUES (6,7,1,1,1,10000.00),(7,7,1,2,1,10000.00),(8,7,1,5,1,10000.00),(9,9,1,NULL,1,10000.00);
+INSERT INTO `order_items` VALUES (1,12,14,2,150000.00),(2,13,16,4,30000.00),(3,14,16,2,30000.00),(4,15,16,2,30000.00),(5,16,16,1,30000.00),(6,17,16,1,30000.00);
 /*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,8 +87,8 @@ CREATE TABLE `orders` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `order_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `total_amount` decimal(10,2) NOT NULL,
-  `status` enum('pending','paid','cancelled') DEFAULT 'pending',
+  `total_amount` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `status` enum('pending','completed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'completed',
   `payment_method` varchar(50) DEFAULT NULL,
   `customer_name` varchar(100) NOT NULL,
   `customer_email` varchar(100) NOT NULL,
@@ -98,7 +96,7 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,7 +105,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (7,3,'2025-04-29 08:08:56',30000.00,'pending','e_wallet','bubudidi','budi@gmail.com','123'),(8,4,'2025-04-29 13:00:31',0.00,'pending','bank_transfer','bobob','bob@gmail.com','123'),(9,4,'2025-04-29 13:00:42',10000.00,'pending','bank_transfer','bobob','bob@gmail.com','123');
+INSERT INTO `orders` VALUES (12,2,'2025-05-03 13:40:59',300000.00,'pending','bank_transfer','budi','budi@gmail.com','085335363218'),(13,5,'2025-05-03 22:57:54',120000.00,'completed','bank_transfer','lavi','lavi@gmail.ocm','08123456'),(14,5,'2025-05-03 23:48:33',60000.00,'completed','bank_transfer','lavi','lavi@gmail.ocm','08123456'),(15,5,'2025-05-04 00:06:05',60000.00,'completed','credit_card','lavi','lavi@gmail.ocm','08123456'),(16,5,'2025-05-04 00:09:31',30000.00,'completed','bank_transfer','lavi','lavi@gmail.ocm','08123456'),(17,5,'2025-05-04 00:19:33',30000.00,'completed','credit_card','lavi','lavi@gmail.ocm','08123456');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +132,7 @@ CREATE TABLE `seats` (
   CONSTRAINT `fk_seats_tickets_match` FOREIGN KEY (`match_id`) REFERENCES `tickets` (`match_id`) ON DELETE CASCADE,
   CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`) ON DELETE CASCADE,
   CONSTRAINT `seats_ibfk_2` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +141,7 @@ CREATE TABLE `seats` (
 
 LOCK TABLES `seats` WRITE;
 /*!40000 ALTER TABLE `seats` DISABLE KEYS */;
-INSERT INTO `seats` VALUES (1,1,1,'A1-001','A1','001','booked'),(2,1,1,'A1-002','A1','002','booked'),(3,1,1,'A1-003','A1','003','available'),(4,1,1,'A2-001','A2','001','available'),(5,1,1,'A2-002','A2','002','available'),(6,1,1,'B1-001','B1','001','available'),(7,1,1,'B1-002','B1','002','available'),(8,1,1,'B1-003','B1','003','booked'),(9,1,1,'B2-001','B2','001','available'),(10,1,1,'B2-002','B2','002','available'),(11,1,1,'C1-001','C1','001','reserved'),(12,1,1,'C1-002','C1','002','reserved'),(13,1,1,'C1-003','C1','003','reserved'),(14,1,1,'C2-001','C2','001','reserved'),(15,1,1,'C2-002','C2','002','reserved'),(16,2,2,'A1-001','A1','001','available'),(17,2,2,'A1-002','A1','002','booked'),(18,2,2,'A1-003','A1','003','available'),(19,2,2,'A2-001','A2','001','booked'),(20,2,2,'A2-002','A2','002','booked'),(21,2,2,'A3001','A3','001','available'),(22,2,2,'A4001','A4','001','available'),(23,2,2,'A5001','A5','001','available'),(24,2,2,'A6001','A6','001','available'),(25,6,6,'A1001','A1','001','available'),(26,6,6,'A2001','A2','001','available');
+INSERT INTO `seats` VALUES (1090,1,14,'A1','A','1','booked'),(1091,1,14,'A2','A','2','available'),(1092,1,14,'A3','A','3','available'),(1093,1,14,'A4','A','4','available'),(1094,1,14,'A5','A','5','available'),(1095,1,14,'A6','A','6','available'),(1096,1,14,'A7','A','7','available'),(1097,1,14,'A8','A','8','available'),(1098,1,14,'A9','A','9','available'),(1099,1,14,'A10','A','10','booked'),(1100,1,14,'A11','A','11','booked'),(1101,1,14,'A12','A','12','booked'),(1102,1,14,'A13','A','13','available'),(1103,1,14,'A14','A','14','available'),(1104,1,14,'A15','A','15','available'),(1105,1,14,'A16','A','16','available'),(1106,1,14,'A17','A','17','available'),(1107,1,14,'A18','A','18','available'),(1108,1,14,'A19','A','19','available'),(1109,1,14,'A20','A','20','available'),(1126,3,16,'A1','A','1','booked'),(1127,3,16,'A2','A','2','booked'),(1128,3,16,'A3','A','3','booked'),(1129,3,16,'A4','A','4','booked'),(1130,3,16,'A5','A','5','available'),(1131,3,16,'A6','A','6','available'),(1132,3,16,'A7','A','7','available'),(1133,3,16,'A8','A','8','available'),(1134,3,16,'A9','A','9','available'),(1135,3,16,'A10','A','10','booked'),(1136,3,16,'A11','A','11','booked'),(1137,3,16,'A12','A','12','booked'),(1138,3,16,'A13','A','13','booked'),(1139,3,16,'A14','A','14','booked'),(1140,3,16,'A15','A','15','booked'),(1141,3,16,'A16','A','16','available'),(1142,3,16,'A17','A','17','available'),(1143,3,16,'A18','A','18','available'),(1144,3,16,'A19','A','19','available'),(1145,3,16,'A20','A','20','available'),(1146,3,16,'B1','B','1','available'),(1147,3,16,'B2','B','2','available'),(1148,3,16,'B3','B','3','available'),(1149,3,16,'B4','B','4','available'),(1150,3,16,'B5','B','5','available');
 /*!40000 ALTER TABLE `seats` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -257,12 +255,12 @@ CREATE TABLE `tickets` (
   `match_id` int NOT NULL,
   `ticket_type` varchar(50) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `quantity_available` int NOT NULL,
+  `quantity_available` int DEFAULT '0',
   `description` text,
   PRIMARY KEY (`id`),
   KEY `match_id` (`match_id`),
   CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -271,7 +269,7 @@ CREATE TABLE `tickets` (
 
 LOCK TABLES `tickets` WRITE;
 /*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-INSERT INTO `tickets` VALUES (1,1,'indo',10000.00,6,'hura'),(2,2,'tiket',15000.00,6,'hura'),(6,6,'Tiket',90000.00,2,'lorem');
+INSERT INTO `tickets` VALUES (14,1,'Thunder FC vs Sky Hawks',150000.00,16,'Big match penentuan juara liga.'),(16,3,'Sky Hawks vs Golden Eagles',30000.00,15,'Pertandingan semi-final yang ditunggu.');
 /*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -294,7 +292,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -303,7 +301,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'budi','$2y$10$DyQTnvUAEkHKW0LoU3mntuN77duWyGHLR.Q9MkE540xEqrUzX.SlO','budi@gmail.com','bubudidi','12345678490',0,'2025-04-29 04:34:00'),(2,'admin','admin','admin@gmail.com','admin','123456789',1,'2025-04-29 05:49:30'),(3,'bubu','$2y$10$UUeV3SnOyayjUvytpMCUe.Q.A.5k6O576anEqLOen959cFdR0e.G2','bubu@gmail.com','bububaba','123',0,'2025-04-29 07:47:15'),(4,'bob','$2y$10$JIy9W/OHJOckuDk/yftaOekQ370zxo42fh2Ve/aaf8UlS/EvMn1ju','bob@gmail.com','bobob','123',0,'2025-04-29 11:35:22');
+INSERT INTO `users` VALUES (1,'budi','$2y$10$DyQTnvUAEkHKW0LoU3mntuN77duWyGHLR.Q9MkE540xEqrUzX.SlO','budi@gmail.com','bubudidi','12345678490',0,'2025-04-29 04:34:00'),(2,'admin','admin','admin@gmail.com','admin','123456789',1,'2025-04-29 05:49:30'),(3,'bubu','$2y$10$UUeV3SnOyayjUvytpMCUe.Q.A.5k6O576anEqLOen959cFdR0e.G2','bubu@gmail.com','bububaba','123',0,'2025-04-29 07:47:15'),(4,'bob','$2y$10$JIy9W/OHJOckuDk/yftaOekQ370zxo42fh2Ve/aaf8UlS/EvMn1ju','bob@gmail.com','bobob','123',0,'2025-04-29 11:35:22'),(5,'lavi','$2y$10$N7pUUhaD2k66r6YEsaM2MOATTny5WBpbire8hKKq1V.eVn8BV42Z6','lavi@gmail.ocm','lavi','081234567',0,'2025-05-03 22:53:01'),(6,'vava','$2y$10$0kCoAEx1YlFTt1FYsi.eaODoAROio6OMr324wkRAG08gA3dwPLI5e','vava@gmail.com','vava','123456789',0,'2025-05-03 23:38:14');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -334,4 +332,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-29 21:46:07
+-- Dump completed on 2025-05-04  7:45:15
